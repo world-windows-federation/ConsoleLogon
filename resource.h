e33c375c20d94ec7d33d94ceb4fca0b035f76dcf
aubymori <aubyomori@gmail.com>
2025-03-04 10:17:46 -0600

    Add stringtable resources

---
+++ resource.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define IDS_CTRLALTDEL        100
+#define IDS_SELECTUSER        101
+#define IDS_SELECTSIGNINOPT   102
+#define IDS_ENTERCREDS        103
+#define IDS_SELECTOPT         104
+#define IDS_USER              105
+#define IDS_OK                106
+#define IDS_CANCEL            107
+#define IDS_YES               108
+#define IDS_NO                109
+#define IDS_LOCK              110
+#define IDS_SIGNOUT           111
+#define IDS_CHANGEPASSWD      112
+#define IDS_TASKMGR           113
+#define IDS_SWITCHUSER        114
+#define IDS_SOMEFORMAT        125
+#define IDS_X                 126
+#define IDS_UNDERSCORE        127
+#define IDS_WINSECURITY       128
+#define IDS_ENTERNEWCREDS     129
+#define IDS_SIGNIN            130
