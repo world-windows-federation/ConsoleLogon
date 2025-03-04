ad8f81cc1d2a027e3d36b4710f3a352d8c0b7951
aubymori <aubyomori@gmail.com>
2025-03-04 13:42:00 -0600

    Clean things up a bit

--- shell/auth/authux/consolelogon/lib/consolelogon.cpp
+++ shell/auth/authux/consolelogon/lib/consolelogon.cpp
@@ -1,7 +1,7 @@
-#include "../dll/pch.h"
+#include "pch.h"
 #include <wrl/implements.h>
 #include <wrl/wrappers/corewrappers.h>
-#include "../../../../../sdk/inc/logoninterfaces.h"
+#include "logoninterfaces.h"
 
 class DECLSPEC_UUID("00000000-0000-0000-0000-000000000000")
 ConsoleLogon 
@@ -11,36 +11,39 @@ ConsoleLogon
 		Microsoft::WRL::FtmBase>
 {
 public:
-	wchar_t* InternalGetRuntimeClassName();
-	enum TrustLevel InternalGetTrustLevel();
-	HRESULT GetRuntimeClassName(struct HSTRING__**);
-	HRESULT GetTrustLevel(enum TrustLevel*);
-	HRESULT GetIids(unsigned long*, struct _GUID**);
-	HRESULT QueryInterface(struct _GUID&, void**);
+	WCHAR* InternalGetRuntimeClassName();
+	TrustLevel InternalGetTrustLevel();
+	HRESULT GetRuntimeClassName(HSTRING*);
+	HRESULT GetTrustLevel(TrustLevel*);
+	HRESULT GetIids(DWORD*, GUID**);
+
+	HRESULT QueryInterface(GUID&, void**);
 	unsigned long Release();
 	unsigned long AddRef();
+
 	ConsoleLogon();
 	HRESULT RuntimeClassInitialize();
-	HRESULT Start(struct IInspectable*, struct Windows::Internal::UI::Logon::Controller::IRedirectionManager*, struct Windows::Internal::UI::Logon::Controller::IUserSettingManager*, struct Windows::Internal::UI::Logon::CredProvData::IDisplayStateProvider*, struct Windows::Internal::UI::Logon::Controller::IBioFeedbackListener*);
-	HRESULT DelayLock(unsigned char, struct Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
-	HRESULT HardLock(enum Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, unsigned char, struct Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
-	HRESULT RequestCredentialsAsync(enum Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, enum Windows::Internal::UI::Logon::Controller::LogonUIFlags, struct ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::RequestCredentialsData*>**);
-	HRESULT ReportCredentialsAsync(enum Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, long, long, struct HSTRING__*, struct HSTRING__*, struct HSTRING__*, struct ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::ReportCredentialsData*>**);
-	HRESULT DisplayMessageAsync(enum Windows::Internal::UI::Logon::Controller::LogonMessageMode, unsigned int, struct HSTRING__*, struct HSTRING__*, struct ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::MessageDisplayResult*>**);
-	HRESULT DisplayCredentialErrorAsync(long, long, unsigned int, struct HSTRING__*, struct HSTRING__*, struct ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::MessageDisplayResult*>**);
-	HRESULT DisplayStatusAsync(enum Windows::Internal::UI::Logon::Controller::LogonUIState, struct HSTRING__*, struct ABI::Windows::Foundation::IAsyncAction**);
-	HRESULT TriggerLogonAnimationAsync(struct ABI::Windows::Foundation::IAsyncAction**);
+	HRESULT Start(IInspectable*, Windows::Internal::UI::Logon::Controller::IRedirectionManager*, Windows::Internal::UI::Logon::Controller::IUserSettingManager*, Windows::Internal::UI::Logon::CredProvData::IDisplayStateProvider*, Windows::Internal::UI::Logon::Controller::IBioFeedbackListener*);
+	HRESULT DelayLock(bool, Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
+	HRESULT HardLock(Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, BYTE, Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
+	HRESULT RequestCredentialsAsync(Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, Windows::Internal::UI::Logon::Controller::LogonUIFlags, ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::RequestCredentialsData*>**);
+	HRESULT ReportCredentialsAsync(Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, long, long, HSTRING, HSTRING, HSTRING, ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::ReportCredentialsData*>**);
+	HRESULT DisplayMessageAsync(Windows::Internal::UI::Logon::Controller::LogonMessageMode, unsigned int, HSTRING, HSTRING, ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::MessageDisplayResult*>**);
+	HRESULT DisplayCredentialErrorAsync(long, long, unsigned int, HSTRING, HSTRING, ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::MessageDisplayResult*>**);
+	HRESULT DisplayStatusAsync(Windows::Internal::UI::Logon::Controller::LogonUIState, HSTRING, ABI::Windows::Foundation::IAsyncAction**);
+	HRESULT TriggerLogonAnimationAsync(ABI::Windows::Foundation::IAsyncAction**);
 	HRESULT ResetCredentials();
 	HRESULT RestoreFromFirstSignInAnimation();
-	HRESULT ClearUIState(struct HSTRING__*);
-	HRESULT ShowSecurityOptionsAsync(enum Windows::Internal::UI::Logon::Controller::LogonUISecurityOptions, struct ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::LogonUISecurityOptionsResult*>**);
-	HRESULT get_WindowContainer(struct IInspectable**);
+	HRESULT ClearUIState(HSTRING);
+	HRESULT ShowSecurityOptionsAsync(Windows::Internal::UI::Logon::Controller::LogonUISecurityOptions, ABI::Windows::Foundation::IAsyncOperation<Windows::Internal::UI::Logon::Controller::LogonUISecurityOptionsResult*>**);
+	HRESULT get_WindowContainer(IInspectable**);
 	HRESULT Hide();
 	HRESULT Stop();
+
 private:
 	~ConsoleLogon();
 	HRESULT CheckUIStarted();
-	HRESULT Lock(enum Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, unsigned char, struct Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
-	class Microsoft::WRL::Wrappers::SRWLock m_Lock;
+	HRESULT Lock(Windows::Internal::UI::Logon::Controller::LogonUIRequestReason, bool, Windows::Internal::UI::Logon::Controller::IUnlockTrigger*);
+	Microsoft::WRL::Wrappers::SRWLock m_Lock;
 	//class Microsoft::WRL::ComPtr<LogonViewManager> m_consoleUIManager;
 };
