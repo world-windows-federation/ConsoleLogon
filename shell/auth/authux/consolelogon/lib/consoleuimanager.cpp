f7aad3a6a86b8ad06137c78e45518f6a8dc7110a
wiktorwiktor12 <68821018+wiktorwiktor12@users.noreply.github.com>
2025-03-05 17:31:21 +0000

    start of consoleuimanager

---
+++ shell/auth/authux/consolelogon/lib/consoleuimanager.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+
+#include <wrl/implements.h>
+#include <wrl/wrappers/corewrappers.h>
+
+#include "InternalAsync.h"
+#include "logoninterfaces.h"
+#include "consoleuimanager.h"
+#include "RefCountedObject.h"
+
+ConsoleUIManager::ConsoleUIManager()
+    : m_continueProcessingInput(true)
+    , m_UIThreadInitResult(E_FAIL)
+{
+}
+
+HRESULT ConsoleUIManager::Initialize()
+{
+}
+
+HRESULT ConsoleUIManager::StartUI()
+{
+}
+
+HRESULT ConsoleUIManager::StopUI()
+{
+}
+
+ConsoleUIManager::~ConsoleUIManager()
+{
+}
+
+HRESULT ConsoleUIManager::SetActiveView(IConsoleUIView*)
+{
+}
+
+HRESULT ConsoleUIManager::EnsureUIStarted()
+{
+}
+
+HRESULT ConsoleUIManager::HandleIncomingInput(INPUT_RECORD)
+{
+}
+
+HRESULT ConsoleUIManager::HandleKeyboardInput(KEY_EVENT_RECORD)
+{
+}
+
+DWORD ConsoleUIManager::s_UIThreadHostStartThreadProc(void*)
+{
+}
+
+HRESULT ConsoleUIManager::UIThreadHostStartThreadProc()
+{
+}
+
+DWORD ConsoleUIManager::s_UIThreadHostThreadProc(void*)
+{
+}
+
+DWORD ConsoleUIManager::UIThreadHostThreadProc()
+{
+}
