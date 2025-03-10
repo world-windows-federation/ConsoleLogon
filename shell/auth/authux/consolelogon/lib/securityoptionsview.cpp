39c91162b0961e6061231c2a4e32d0317557898b
Amrsatrio <satrionareswara@yahoo.com>
2025-03-11 01:50:45 +0700

    Review SecurityOptionsView

--- shell/auth/authux/consolelogon/lib/securityoptionsview.cpp
+++ shell/auth/authux/consolelogon/lib/securityoptionsview.cpp
@@ -1,11 +1,7 @@
 ﻿#include "pch.h"
 
-#include "userselectionview.h"
-
-#include "basictextcontrol.h"
 #include "securityoptionsview.h"
 
-#include "resource.h"
 #include "securityoptioncontrol.h"
 
 using namespace Microsoft::WRL;
@@ -26,35 +22,35 @@ HRESULT SecurityOptionsView::RuntimeClassInitialize(LC::LogonUISecurityOptions o
 	if ((options & LC::LogonUISecurityOptions_ChangePassword) != 0)
 	{
 		ComPtr<SecurityOptionControl> optionControl;
-		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_ChangePassword,&completion)); // 28
+		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_ChangePassword, &completion)); // 28
 	}
 
 	if ((options & LC::LogonUISecurityOptions_Lock) != 0)
 	{
 		ComPtr<SecurityOptionControl> optionControl;
-		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_Lock,&completion)); // 34
+		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_Lock, &completion)); // 34
 	}
 
 	if ((options & LC::LogonUISecurityOptions_LogOff) != 0)
 	{
 		ComPtr<SecurityOptionControl> optionControl;
-		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_LogOff,&completion)); // 40
+		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_LogOff, &completion)); // 40
 	}
 
 	if ((options & LC::LogonUISecurityOptions_SwitchUser) != 0)
 	{
 		ComPtr<SecurityOptionControl> optionControl;
-		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_SwitchUser,&completion)); // 46
+		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_SwitchUser, &completion)); // 46
 	}
 
 	if ((options & LC::LogonUISecurityOptions_TaskManager) != 0)
 	{
 		ComPtr<SecurityOptionControl> optionControl;
-		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_TaskManager,&completion)); // 52
+		RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_TaskManager, &completion)); // 52
 	}
 
 	ComPtr<SecurityOptionControl> optionControl;
-	RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl,this,LC::LogonUISecurityOptions_Cancel,&completion)); // 56
+	RETURN_IF_FAILED(MakeAndInitialize<SecurityOptionControl>(&optionControl, this, LC::LogonUISecurityOptions_Cancel, &completion)); // 56
 
 	m_completion = wil::make_unique_nothrow<WI::AsyncDeferral<WI::CMarshaledInterfaceResult<LC::ILogonUISecurityOptionsResult>>>(completion);
 	RETURN_HR_IF_NULL(E_OUTOFMEMORY, m_completion.get()); // 59
@@ -70,16 +66,18 @@ HRESULT SecurityOptionsView::v_OnKeyInput(const KEY_EVENT_RECORD* keyEvent, BOOL
 		return S_OK;
 
 	ComPtr<LC::ILogonUISecurityOptionsResultFactory> factory;
-	RETURN_IF_FAILED(WF::GetActivationFactory(Microsoft::WRL::Wrappers::HStringReference(RuntimeClass_Windows_Internal_UI_Logon_Controller_LogonUISecurityOptionsResult).Get(), &factory)); // 70
+	RETURN_IF_FAILED(WF::GetActivationFactory(
+		Microsoft::WRL::Wrappers::HStringReference(RuntimeClass_Windows_Internal_UI_Logon_Controller_LogonUISecurityOptionsResult).Get(), &factory)); // 70
 
 	ComPtr<LC::ILogonUISecurityOptionsResult> optionResult;
 	RETURN_IF_FAILED(factory->CreateSecurityOptionsResult(LC::LogonUISecurityOptions_Cancel, LC::LogonUIShutdownChoice_None, &optionResult)); // 73
-	
+
 	// more bad code from them, not fixing it yet just in case the badness is needed
 	WI::CMarshaledInterfaceResult<LC::ILogonUISecurityOptionsResult> result = m_completion->GetResult();
 	RETURN_IF_FAILED(result.Set(optionResult.Get())); // 76
 
 	m_completion->Complete(S_OK);
+	m_completion.reset();
 
 	*wasHandled = TRUE;
 	return S_OK;
